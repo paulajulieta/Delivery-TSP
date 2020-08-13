7b1e0d1abb1c1d4d8ef248afdebdc3f68978a664
AndreTati <andrea.duran@alumnos.frm.utn.edu.ar>
2020-08-13 20:42:19 -0300

    Diseñando Modal Registro

--- DeliveryCliente/src/app/pages/home/home.component.ts
+++ DeliveryCliente/src/app/pages/home/home.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  images:Array<string>;
   constructor() { }
 
   ngOnInit(): void {
+    this.images=["./assets/images/pizza-clasica.png", "./assets/images/hamburguesa1.png", "./assets/images/empanadas.jpg"];
   }
 
 }
